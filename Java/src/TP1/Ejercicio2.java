525797a4375323c1d0666d8803bbe682fcced1bd
ferroxido <ferroxido@gmail.com>
2013-09-17 20:42:47 -0300

    tp1ejer2

--- Java/src/TP1/Ejercicio2.java
+++ Java/src/TP1/Ejercicio2.java
@@ -2,9 +2,10 @@ package TP1;
 
 public class Ejercicio2 {
 	
+	private final int[][] automataUnTercio = {{1,3},{2,0},{2,2},{3,3}};
+	private final int[][] automataUnCuarto = {{1,3},{2,3},{2,2},{3,3}};
 	
-	private final int[][] matrizDeEstados1 = {{1,3}, {4,2}, {2,3}, {3,3}, {4,4}};
-	private final int[][] matrizDeEstados2 = {{1,2}, {3,0}, {2,2}, {3,3}};
+	
 	
 	private int cola = 0, tOcioso = 0, tTotal = 0, tEspera = 0, clientes = 0;
 	
@@ -13,24 +14,28 @@ public class Ejercicio2 {
 	private float T = 0.0f;//Tiempo de simulacion en minutos
 	
 	
-	
+	/*
+	 * Obtiene el siguiente estado resultante del automta 1/4
+	 */
 	public int nuevoEstado1(int estadoActual, String c){
 		
 		if(c.equals("C")){
-			return matrizDeEstados1[estadoActual][0];
+			return automataUnCuarto[estadoActual][0];
 		}else{
-			return matrizDeEstados1[estadoActual][1];
+			return automataUnCuarto[estadoActual][1];
 		}
 		
 	}
 	
-	
+	/*
+	 * Obtiene el siguiente estado resultante del automta 1/3
+	 */
 	public int nuevoEstado2(int estadoActual, String c){
 		
 		if(c.equals("C")){
-			return matrizDeEstados2[estadoActual][0];
+			return automataUnTercio[estadoActual][0];
 		}else{
-			return matrizDeEstados2[estadoActual][1];
+			return automataUnTercio[estadoActual][1];
 		}
 		
 	}
@@ -43,16 +48,18 @@ public class Ejercicio2 {
 			T++;
 			if(cola > 0){
 				estadoActual = 0;
+				//decido si le doy un minuto más de servicio
 				while(estadoActual != 2 && estadoActual != 3 && cadena.length() > 0){
 					estadoActual = nuevoEstado2(estadoActual, cadena.substring(0, 1));
 					cadena = cadena.substring(1, cadena.length());			
 				}
-				if(estadoActual != 2){
+				if(estadoActual != 3){
 					cola--;
 				}
 			}
 			estadoActual = 0;
-			while(estadoActual != 3 && estadoActual != 4 && cadena.length() > 0){
+			//decido la llegada de un nuevo cliente
+			while(estadoActual != 2 && estadoActual != 3 && cadena.length() > 0){
 				estadoActual = nuevoEstado1(estadoActual, cadena.substring(0, 1));
 				cadena = cadena.substring(1, cadena.length());			
 			}
